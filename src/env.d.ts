865ede38570d256766e2aa59ccac7ba04701941b
TRIANA115 <andresfelipetriana443@gmail.com>
2025-08-02 14:36:10 -0500

    se hiso la vista de  Inicio de Sesión

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="astro/client" />
+
+declare module '*.astro' {
+  import type { AstroComponentFactory } from 'astro/dist/runtime/server';
+  const Component: AstroComponentFactory;
+  export default Component;
+}
